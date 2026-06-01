236aee9952b525e3236ece5a060196e21ace8d70
jessyrand <henintsoarandriam@gmail.com>
2026-06-01 07:27:12 +0300

    feat: fix dataProvider Resource URL

--- src/providers/dataProvider.ts
+++ src/providers/dataProvider.ts
@@ -2,9 +2,13 @@ import type { DataProvider } from "react-admin";
 import { env } from "../config/env";
 import { httpClient } from "./httpClient";
 
+const getResourceUrl = (resource: string) => {
+    return `${env.apiUrl}/admin/${resource}`;
+};
+
 export const dataProvider: DataProvider = {
     getList: async (resource) => {
-        const data = await httpClient(`${env.apiUrl}/${resource}`);
+        const data = await httpClient(getResourceUrl(resource));
 
         return {
             data,
@@ -13,7 +17,29 @@ export const dataProvider: DataProvider = {
     },
 
     getOne: async (resource, params) => {
-        const data = await httpClient(`${env.apiUrl}/${resource}/${params.id}`);
+        const data = await httpClient(`${getResourceUrl(resource)}/${params.id}`);
+
+        return {
+            data,
+        };
+    },
+
+    create: async (resource, params) => {
+        const data = await httpClient(getResourceUrl(resource), {
+            method: "POST",
+            body: JSON.stringify(params.data),
+        });
+
+        return {
+            data,
+        };
+    },
+
+    update: async (resource, params) => {
+        const data = await httpClient(`${getResourceUrl(resource)}/${params.id}`, {
+            method: "PUT",
+            body: JSON.stringify(params.data),
+        });
 
         return {
             data,
@@ -28,23 +54,15 @@ export const dataProvider: DataProvider = {
         throw new Error("getManyReference is not implemented yet");
     },
 
-    create: async () => {
-        throw new Error("create is not implemented yet");
-    },
-
-    update: async () => {
-        throw new Error("update is not implemented yet");
-    },
-
     updateMany: async () => {
         throw new Error("updateMany is not implemented yet");
     },
 
     delete: async () => {
-        throw new Error("delete is not implemented yet");
+        throw new Error("delete is not implemented");
     },
 
     deleteMany: async () => {
-        throw new Error("deleteMany is not implemented yet");
+        throw new Error("deleteMany is not implemented");
     },
 };
