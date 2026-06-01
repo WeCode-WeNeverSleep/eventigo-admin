f59339906d9516737fb78d358162b20fc2970c86
jessyrand <henintsoarandriam@gmail.com>
2026-06-01 07:38:15 +0300

    feat: add Edit Button to RoomList

--- src/resources/rooms/RoomList.tsx
+++ src/resources/rooms/RoomList.tsx
@@ -1,4 +1,4 @@
-import { Datagrid, List, TextField } from "react-admin";
+import { Datagrid, EditButton, List, TextField } from "react-admin";
 
 export function RoomList() {
     return (
@@ -6,6 +6,7 @@ export function RoomList() {
             <Datagrid rowClick="edit">
                 <TextField source="id" />
                 <TextField source="name" />
+                <EditButton />
             </Datagrid>
         </List>
     );
