31957800fd2222e10f75a9b40bcc639b460c056b
jessyrand <henintsoarandriam@gmail.com>
2026-06-14 07:40:53 +0300

    feat: add dashboard overview

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,10 @@ import { EventShow } from "./resources/events/EventShow";
 import { darkTheme, lightTheme } from "./theme/adminTheme";
 import { AdminLayout } from "./layout/AdminLayout";
 
+import { Dashboard } from "./dashboard/Dashboard";
+
 export const App = () => (
-    <Admin dataProvider={dataProvider} authProvider={authProvider} theme={lightTheme} darkTheme={darkTheme} defaultTheme="dark" layout={AdminLayout}>
+    <Admin dataProvider={dataProvider} authProvider={authProvider} theme={lightTheme} darkTheme={darkTheme} defaultTheme="dark" layout={AdminLayout} dashboard={Dashboard}>
         <Resource
             name="events"
             list={EventList}
