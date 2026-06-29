20e65fe65b7fd4d9a3746e920cc1f559517bdeae
jessyrand <henintsoarandriam@gmail.com>
2026-06-29 15:07:29 +0300

    feat: add room delete

--- src/resources/rooms/RoomEdit.tsx
+++ src/resources/rooms/RoomEdit.tsx
@@ -1,9 +1,10 @@
 import { Edit, SimpleForm, TextInput } from "react-admin";
+import { AdminToolbar } from "../../components/AdminToolbar";
 
 export function RoomEdit() {
     return (
         <Edit redirect="list">
-            <SimpleForm>
+            <SimpleForm toolbar={<AdminToolbar />}>
                 <TextInput source="name" label="Name" required />
             </SimpleForm>
         </Edit>
